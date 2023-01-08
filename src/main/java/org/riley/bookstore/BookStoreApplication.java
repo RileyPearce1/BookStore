d8bca216f49f744d4678b03de745721c9dc9cef3
Alexander Urumov <sahaheh@mail.ru>
2023-01-08 22:47:30 +0800

    test ci/cd

--- src/main/java/org/riley/bookstore/BookStoreApplication.java
+++ src/main/java/org/riley/bookstore/BookStoreApplication.java
@@ -1,13 +1,16 @@
 package org.riley.bookstore;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(BookStoreApplication.class)
+                .run(args);
+
+    }
 
 }
--- src/main/java/org/riley/bookstore/securingweb/SecuringWebApplication.java
+++ src/main/java/org/riley/bookstore/securingweb/SecuringWebApplication.java
@@ -3,11 +3,11 @@ package org.riley.bookstore.securingweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SecuringWebApplication {
 
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         SpringApplication.run(SecuringWebApplication.class, args);
-    }
+    }*/
 
 }
